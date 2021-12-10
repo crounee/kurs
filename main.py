58c8dd185a89b7906a406c51c367cbfb9abb600c
crounee <60732262+crounee@users.noreply.github.com>
2021-12-10 13:43:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,8 @@ class Main_window(QtWidgets.QMainWindow):
         self.info_7 = QtWidgets.QLabel('info 7')
 
 
+        self.but_all = QtWidgets.QPushButton('Подробная информация')
+        self.but_all.clicked.connect(self.all_information_bd)
 
         self.lay1.addWidget(self.push_but_1)
         self.lay1.addWidget(self.info_1)
@@ -104,6 +106,8 @@ class Main_window(QtWidgets.QMainWindow):
         self.lay1.addWidget(self.push_but_7)
         self.lay1.addWidget(self.info_7)
 
+        self.lay1.addWidget(self.but_all)
+
         scrol = QtWidgets.QScrollArea()
         scrol.setWidget(self.widget)
         scrol.setWidgetResizable(True)
@@ -114,15 +118,228 @@ class Main_window(QtWidgets.QMainWindow):
 
     def all_information_bd(self):
         self.all_info = QtWidgets.QMainWindow()
-        self.widget4 = QtWidgets.QWidget()
-        self.lay5 = QtWidgets.QVBoxLayout(self.widget4)
+        self.all_info.setWindowTitle('Строительная компания')
+        self.all_info.setWindowIcon(QtGui.QIcon('1.jpg'))
+        self.widget_info = QtWidgets.QWidget()
+        self.lay_info = QtWidgets.QVBoxLayout(self.widget_info)
+        ####start
 
-        self.brigades = QtWidgets.QLabel('brigades')
-        self.brigades_information = QtWidgets.QLabel('Info')
-        self.brigades_input = QtWidgets.QLineEdit('')
+        self.table_1 = QtWidgets.QTableWidget()
+        self.table_1.setColumnCount(4)
+        self.table_1.setRowCount(len(self.get_info_brigades()))
+        self.table_1.setHorizontalHeaderLabels(['code_brigades','code_employee_1','code_employee_2','code_employee_3'])
+        self.but_update_table_1 = QtWidgets.QPushButton('Обновить brigadees')
+        self.but_update_table_1.clicked.connect(self.update_brigades)
+
+
+        self.lay_info.addWidget(self.table_1)
+        self.lay_info.addWidget(self.but_update_table_1)
+
+
+        self.table_2 = QtWidgets.QTableWidget()
+        self.table_2.setColumnCount(5)
+        self.table_2.setRowCount(len(self.get_info_customers()))
+        self.table_2.setHorizontalHeaderLabels(['code_customer','fio','	adres','phone','passport'])
+        self.but_update_table_2 = QtWidgets.QPushButton('обновить customers')
+        self.but_update_table_2.clicked.connect(self.update_customers)
+
+        self.lay_info.addWidget(self.table_2)
+        self.lay_info.addWidget(self.but_update_table_2)
 
 
 
+        self.table_3 = QtWidgets.QTableWidget()
+        self.table_3.setColumnCount(5)
+        self.table_3.setRowCount(len(self.get_info_materials()))
+        self.table_3.setHorizontalHeaderLabels(['code_customer', 'fio', 'adres', 'phone', 'passport'])
+        self.but_update_table_3 = QtWidgets.QPushButton('обновить materials')
+        self.but_update_table_3.clicked.connect(self.update_materials)
+
+        self.lay_info.addWidget(self.table_3)
+        self.lay_info.addWidget(self.but_update_table_3)
+
+
+
+        self.table_4 = QtWidgets.QTableWidget()
+        self.table_4.setColumnCount(9)
+        self.table_4.setRowCount(len(self.get_info_orders()))
+        self.table_4.setHorizontalHeaderLabels(['code_customer', 'code_type', 'code_brigades', 'coast', 'date_start','date_end','completion_mark','pay_mark','code_staff'])
+        self.but_update_table_4 = QtWidgets.QPushButton('обновить orders')
+        self.but_update_table_4.clicked.connect(self.update_orders)
+
+        self.lay_info.addWidget(self.table_4)
+        self.lay_info.addWidget(self.but_update_table_4)
+
+
+
+        self.table_5 = QtWidgets.QTableWidget()
+        self.table_5.setColumnCount(5)
+        self.table_5.setRowCount(len(self.get_info_positions()))
+        self.table_5.setHorizontalHeaderLabels(['code_position', 'name_position', 'salary', 'responsibilities', 'requirements'])
+        self.but_update_table_5 = QtWidgets.QPushButton('обновить positions')
+        self.but_update_table_5.clicked.connect(self.update_positions)
+
+        self.lay_info.addWidget(self.table_5)
+        self.lay_info.addWidget(self.but_update_table_5)
+
+
+        self.table_6 = QtWidgets.QTableWidget()
+        self.table_6.setColumnCount(8)
+        self.table_6.setRowCount(len(self.get_info_staff()))
+        self.table_6.setHorizontalHeaderLabels(['code_staff', 'fio', 'age', 'gender', '	adres', 'phone', 'passport','code_position'])
+        self.but_update_table_6 = QtWidgets.QPushButton('обновить staff')
+        self.but_update_table_6.clicked.connect(self.update_staff)
+
+        self.lay_info.addWidget(self.table_6)
+        self.lay_info.addWidget(self.but_update_table_6)
+
+
+        self.table_7 = QtWidgets.QTableWidget()
+        self.table_7.setColumnCount(7)
+        self.table_7.setRowCount(len(self.get_info_types_of_works()))
+        self.table_7.setHorizontalHeaderLabels(['code_type', 'name', 'description', 'coast_of_work', 'code_material_1', 'code_material_2', 'code_material_3'])
+        self.but_update_table_7 = QtWidgets.QPushButton('обновить types_of_works')
+        self.but_update_table_7.clicked.connect(self.update_types_of_works)
+
+        self.lay_info.addWidget(self.table_7)
+        self.lay_info.addWidget(self.but_update_table_7)
+
+
+        ####end
+        self.new_scrol = QtWidgets.QScrollArea()
+        self.new_scrol.setWidget(self.widget_info)
+        self.new_scrol.setWidgetResizable(True)
+
+        self.all_info.setCentralWidget(self.new_scrol)
+        self.all_info.resize(800,600)
+        self.all_info.show()
+
+##brigadees
+    def get_info_brigades(self):
+        with connections.Connection(user='root', host='localhost', database='kurs') as bd:
+            with cursors.Cursor(bd) as cursor:
+                sql = ('SELECT * FROM `brigades`')
+                cursor.execute(sql)
+                g = cursor.fetchall()
+
+            return g
+
+
+    def update_brigades(self):
+        for i in range(0,len(self.get_info_brigades())):
+            for g in range(0,4):
+                f = str(self.get_info_brigades()[i][g])
+
+                self.table_1.setItem(i,g,QtWidgets.QTableWidgetItem(f))
+
+###customers
+    def get_info_customers(self):
+        with connections.Connection(user='root', host='localhost', database='kurs') as bd:
+            with cursors.Cursor(bd) as cursor:
+                sql = ('SELECT * FROM `customers`')
+                cursor.execute(sql)
+                g = cursor.fetchall()
+
+            return g
+
+
+    def update_customers(self):
+        for i in range(0,len(self.get_info_customers())):
+            for g in range(0,5):
+                f = str(self.get_info_customers()[i][g])
+
+                self.table_2.setItem(i,g,QtWidgets.QTableWidgetItem(f))
+
+###materials
+    def get_info_materials(self):
+        with connections.Connection(user='root', host='localhost', database='kurs') as bd:
+            with cursors.Cursor(bd) as cursor:
+                sql = ('SELECT * FROM `materials`')
+                cursor.execute(sql)
+                g = cursor.fetchall()
+
+            return g
+
+
+    def update_materials(self):
+        for i in range(0,len(self.get_info_materials())):
+            for g in range(0,5):
+                f = str(self.get_info_materials()[i][g])
+
+                self.table_3.setItem(i,g,QtWidgets.QTableWidgetItem(f))
+
+###orders
+    def get_info_orders(self):
+        with connections.Connection(user='root', host='localhost', database='kurs') as bd:
+            with cursors.Cursor(bd) as cursor:
+                sql = ('SELECT * FROM `orders`')
+                cursor.execute(sql)
+                g = cursor.fetchall()
+
+            return g
+
+
+    def update_orders(self):
+        for i in range(0,len(self.get_info_orders())):
+            for g in range(0,9):
+                f = str(self.get_info_orders()[i][g])
+
+                self.table_4.setItem(i,g,QtWidgets.QTableWidgetItem(f))
+
+
+###  positions
+    def get_info_positions(self):
+        with connections.Connection(user='root', host='localhost', database='kurs') as bd:
+            with cursors.Cursor(bd) as cursor:
+                sql = ('SELECT * FROM `positions`')
+                cursor.execute(sql)
+                g = cursor.fetchall()
+
+            return g
+
+
+    def update_positions(self):
+        for i in range(0,len(self.get_info_positions())):
+            for g in range(0,5):
+                f = str(self.get_info_positions()[i][g])
+
+                self.table_5.setItem(i,g,QtWidgets.QTableWidgetItem(f))
+
+###staff
+    def get_info_staff(self):
+        with connections.Connection(user='root', host='localhost', database='kurs') as bd:
+            with cursors.Cursor(bd) as cursor:
+                sql = ('SELECT * FROM `staff`')
+                cursor.execute(sql)
+                g = cursor.fetchall()
+
+            return g
+
+
+    def update_staff(self):
+        for i in range(0,len(self.get_info_staff())):
+            for g in range(0,8):
+                f = str(self.get_info_staff()[i][g])
+
+                self.table_6.setItem(i,g,QtWidgets.QTableWidgetItem(f))
+
+###types_of_works
+    def get_info_types_of_works(self):
+        with connections.Connection(user='root', host='localhost', database='kurs') as bd:
+            with cursors.Cursor(bd) as cursor:
+                sql = ('SELECT * FROM `types_of_works`')
+                cursor.execute(sql)
+                g = cursor.fetchall()
+
+            return g
+
+
+    def update_types_of_works(self):
+        for i in range(0,len(self.get_info_types_of_works())):
+            for g in range(0,7):
+                f = str(self.get_info_types_of_works()[i][g])
+
+                self.table_7.setItem(i,g,QtWidgets.QTableWidgetItem(f))
 
     def forms(self):
         self.form = QtWidgets.QMainWindow()
@@ -239,6 +456,11 @@ class Main_window(QtWidgets.QMainWindow):
                 g = cursor.fetchall()
                 print(g)
                 self.info_7.setText(str(g))
+
+
+
+
+
 class App(QtWidgets.QApplication):
     def __init__(self,argv):
         super().__init__(argv)
